b44723e50481c62c2351ca02bab91928624411f3
ShiXing Wang <wsxbj985@gmail.com>
2026-01-06 14:52:39 +0800

    取消合作修改

--- frontend/src/pages/Cooperation.vue
+++ frontend/src/pages/Cooperation.vue
@@ -286,7 +286,7 @@ const handleReject = async (coop) => {
     })
 
     // 根据角色调用对应的拒绝API
-    if (coop.role === 0) {
+    if (coop.role == 0) {
       // 邀请：学生拒绝邀请
       await rejectInvitation({ cooperation_id: coop.cooperation_id })
     } else {
